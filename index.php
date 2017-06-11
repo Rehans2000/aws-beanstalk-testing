e847ddfa8a5de51eb50e1e6d5e31ce194f42df7e
Rehan <networks@accunity.com>
2017-06-11 17:30:50 +0500

    edit index

--- index.php
+++ index.php
@@ -1,14 +1,1 @@
-<?
-</head>
-<body>
-    <section class="congratulations">
-        <h1>AWS CODE-DEPLOY-</h1>
-        <p>Your Code Deploy Appk <em>PHP</em> code deploy application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
-        <p>You are running PHP 7.0 version <?= phpversion() ?></p>
-    </section>
-
-</body>
-</html>
-<? 
-} 
-?>
+aws code deploy
