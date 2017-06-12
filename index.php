94326b4fc821228af7990a0e852f420fc3e11e03
Rehans2000 <rehans2k3@gmail.com>
2017-06-12 10:29:45 +0500

    Create index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>AWS CODE-DEPLOY-</h1>
+        <h1>AWS CODE-DEPLOY-Via Github</h1>
         <p>Your Code Deploy Appk <em>PHP</em> code deploy application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP 7.0 & httpd 2.4version <?= phpversion() ?></p>
     </section>
