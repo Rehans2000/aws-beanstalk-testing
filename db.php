38b9f751cd63f01c478d02861327e277afa113a5
Rehans2000 <rehans2k3@gmail.com>
2017-06-12 11:18:46 +0500

    Create db.php

--- db.php
+++ db.php
@@ -5,6 +5,6 @@ $hostname = "dev-rds-a2z.cmiru3hmgmgq.eu-central-1.rds.amazonaws.com";
 
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) 
-  or die("Unable to connect to MySQL");
+  or die("Unable to connecdffdfdt to MySQL");
 echo "Connected to MySQL<br>";
 ?>
