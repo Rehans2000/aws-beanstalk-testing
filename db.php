fea5d1307e0b6c45d21711248d5c18fe45b72e69
Rehans2000 <rehans2k3@gmail.com>
2017-06-18 17:36:52 +0500

    test

--- db.php
+++ db.php
@@ -1,5 +1,25 @@
 <?php
-ttottotoot
+$use1rname = "master";
+$password = "Masterrds123";dfadfdfadf
+$hostname = "dev-rds-a2z.cmiru3hmgmgq.eu-central-1.rds.amazonaws.com"; 
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+  or die("Unable to connecdffdfdt to MySQL");
+echo "Connected to MySQL<br>";
+?>
+<?php
+$use1rname = "master";
+$password = "Masterrds123";dfadfdfadf
+$hostname = "dev-rds-a2z.cmiru3hmgmgq.eu-central-1.rds.amazonaws.com"; 
+
+//connection to the database
+$dbhandle = mysql_connect($hostname, $username, $password) 
+  or die("Unable to connecdffdfdt to MySQL");
+echo "Connected to MySQL<br>";
+?>
+
+<?php
 $use1rname = "master";
 $password = "Masterrds123";dfadfdfadf
 $hostname = "dev-rds-a2z.cmiru3hmgmgq.eu-central-1.rds.amazonaws.com"; 
